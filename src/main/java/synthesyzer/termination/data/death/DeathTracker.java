c31ff5bc86c10149a606f99f4ec6fc78e3b31215
DaanSchar <Daan.Schaer@Gmail.com>
2023-07-09 14:26:10 +0200

    doc: added documentation for DeathTracker

--- src/main/java/synthesyzer/termination/data/death/DeathTracker.java
+++ src/main/java/synthesyzer/termination/data/death/DeathTracker.java
@@ -20,6 +20,9 @@ public class DeathTracker extends PersistentState {
         super();
     }
 
+    /**
+     * Gets the death tracker from a world
+     */
     public static DeathTracker get(World world) {
         if (world instanceof ServerWorld serverWorld) {
             var stateManager = serverWorld.getPersistentStateManager();
@@ -29,6 +32,9 @@ public class DeathTracker extends PersistentState {
         throw new RuntimeException("Can't access data from client side");
     }
 
+    /**
+     * Creates a death tracker from a saved tag
+     */
     public DeathTracker(NbtCompound savedTag) {
         NbtList deathTimersTagList = savedTag.getList("deathTimers", NbtElement.COMPOUND_TYPE);
 
@@ -40,6 +46,11 @@ public class DeathTracker extends PersistentState {
         });
     }
 
+    /**
+     * Writes the death tracker to a compunt tag
+     * @param nbt the tag to write to
+     * @return the tag with all death timers
+     */
     @Override
     public NbtCompound writeNbt(NbtCompound nbt) {
         NbtList listTag = new NbtList();
@@ -55,6 +66,9 @@ public class DeathTracker extends PersistentState {
         return nbt;
     }
 
+    /**
+     * Updates the death timers of all currently dead players
+     */
     public void update() {
         deathTimers.forEach((gameProfile, remainingTicks) -> {
             if (remainingTicks > 0) {
@@ -64,21 +78,37 @@ public class DeathTracker extends PersistentState {
         setDirty(true);
     }
 
+    /**
+     * Adds a player to the death tracker
+     * @param playerId the id of the player to add
+     */
     public void addPlayerDeath(UUID playerId) {
         final int ticksPerSecond = 20;
         deathTimers.put(playerId, Termination.CONFIG.playerDeathCooldown() * ticksPerSecond);
         setDirty(true);
     }
 
+    /**
+     * Removes a player from the death tracker, meaning they are no longer dead
+     * @param playerId the id of the player to remove
+     */
     public void removePlayerDeath(UUID playerId) {
         deathTimers.remove(playerId);
         setDirty(true);
     }
 
+    /**
+     * Gets the death timers
+     * @return map of death timers, with the key being the player id and the value being the remaining ticks
+     */
     public HashMap<UUID, Integer> getDeathTimers() {
         return deathTimers;
     }
 
+    /**
+     * Gets all players that are currently dead and their death timer is 0
+     * @return list of player ids
+     */
     public List<UUID> getPlayersToBeRevived() {
         return deathTimers.entrySet().stream()
                 .filter(entry -> entry.getValue() <= 0)
@@ -86,10 +116,20 @@ public class DeathTracker extends PersistentState {
                 .toList();
     }
 
+    /**
+     * Checks if a player is dead
+     * @param id the id of the player to check
+     * @return true if the player is dead, false otherwise
+     */
     public boolean isPlayerDead(UUID id) {
         return deathTimers.containsKey(id) && deathTimers.get(id) > 0;
     }
 
+    /**
+     * Gets the remaining ticks of a player's death timer
+     * @param id the id of the player to check
+     * @return the remaining ticks of the player's death timer
+     */
     public int getDeathTimer(UUID id) {
         return deathTimers.get(id);
     }
