d0381536d5484367742e2458710c12637c9414eb
DaanSchar <Daan.Schaer@Gmail.com>
2023-07-09 14:42:24 +0200

    fix: test ticks_per_second back to 20

--- src/main/java/synthesyzer/termination/util/Time.java
+++ src/main/java/synthesyzer/termination/util/Time.java
@@ -2,7 +2,7 @@ package synthesyzer.termination.util;
 
 public class Time {
 
-    public static final int TICKS_PER_SECOND = 5;
+    public static final int TICKS_PER_SECOND = 20;
     public static final int TICKS_PER_MINUTE = TICKS_PER_SECOND * 60;
 
 }
