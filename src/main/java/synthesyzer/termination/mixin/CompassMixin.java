e0d6d14008838e359fdc04fab5ca270afa129e16
DaanSchar <Daan.Schaer@Gmail.com>
2023-07-13 21:41:57 +0200

    feat: compasses now track closest players from other teams

---
+++ src/main/java/synthesyzer/termination/mixin/CompassMixin.java
@@ -0,0 +1,73 @@
+package synthesyzer.termination.mixin;
+
+import net.minecraft.client.item.CompassAnglePredicateProvider;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.Entity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(CompassAnglePredicateProvider.class)
+public abstract class CompassMixin {
+
+    @Shadow protected abstract float getAngleTo(Entity entity, long time, BlockPos pos);
+
+    @Inject(method = "getAngle", at = @At("TAIL"), cancellable = true)
+    public void getAngle(ItemStack stack, ClientWorld world, int seed, Entity entity, CallbackInfoReturnable<Float> cir) {
+        var ourCoord = entity.getPos();
+
+        if (entity.getScoreboardTeam() == null) {
+            return;
+        }
+
+        AbstractClientPlayerEntity closestPlayer = null;
+
+        if (world.getPlayers().size() <= 1) {
+            cir.setReturnValue(0.0F);
+            return;
+        }
+
+        for(AbstractClientPlayerEntity player : world.getPlayers()) {
+            if (entity.equals(player)) {
+                continue;
+            }
+
+            if (player.getScoreboardTeam() == null) {
+                continue;
+            }
+
+            if (player.isCreative() || player.isSpectator()) {
+                continue;
+            }
+
+            var playerCoord = player.getPos();
+
+            if (entity.getScoreboardTeam().equals(player.getScoreboardTeam())) {
+                continue;
+            }
+
+            if (closestPlayer == null) {
+                closestPlayer = player;
+            }
+
+            if (ourCoord.distanceTo(playerCoord) < ourCoord.distanceTo(closestPlayer.getPos())) {
+                closestPlayer = player;
+            }
+        }
+
+        if (closestPlayer == null) {
+            cir.setReturnValue(0.0F);
+            return;
+        }
+
+        float angle = getAngleTo(entity, world.getTime(),closestPlayer.getBlockPos());
+
+        cir.setReturnValue(angle);
+    }
+
+}
