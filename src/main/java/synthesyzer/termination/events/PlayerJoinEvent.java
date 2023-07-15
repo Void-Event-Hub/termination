ba207e2c181b26479c16037b0b080e1c6c5bc3c1
DaanSchar <Daan.Schaer@Gmail.com>
2023-07-15 14:03:55 +0200

    chore: added join message

--- src/main/java/synthesyzer/termination/events/PlayerJoinEvent.java
+++ src/main/java/synthesyzer/termination/events/PlayerJoinEvent.java
@@ -5,6 +5,7 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import synthesyzer.termination.data.team.TeamDataManager;
 import synthesyzer.termination.network.TMNetwork;
 import synthesyzer.termination.network.packets.servertoclient.UpdateTeamDataPacket;
+import synthesyzer.termination.util.Messenger;
 
 public class PlayerJoinEvent {
 
@@ -12,6 +13,17 @@ public class PlayerJoinEvent {
         ServerEntityEvents.ENTITY_LOAD.register((entity, world) -> {
             if (entity instanceof ServerPlayerEntity player) {
                 TMNetwork.CHANNEL.serverHandle(player).send(new UpdateTeamDataPacket(TeamDataManager.get(world).getTeamData()));
+                Messenger.sendMessage(player, """
+
+                        §7Termination's 3 Phases:\s
+                        §eStart Phase§7: You're free to roam anywhere, but you can't destroy the enemy nucleus.\s
+
+                        §6Phase 2§7: You can now destroy the enemy nucleus.
+
+                        §eEnd Phase§7: All nuclei die, meaning you have no respawns.\s
+
+                        §8Destroy the enemy nucleus and kill all players to win!
+                        """);
             }
         });
 
